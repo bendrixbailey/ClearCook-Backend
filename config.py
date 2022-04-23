915cf1cb4960596eea486cfdae6b1dee4c82cf58
bnb4505 <bnb4505@g.rit.edu>
2022-04-22 20:18:51 -0400

    working on basic functions

---
+++ config.py
@@ -0,0 +1,10 @@
+#port: 3306
+
+import os
+
+settings = {
+    'host' : 'clearcookdb.mongo.cosmos.azure.com',
+    'username' : 'clearcookdb',
+    'primary_key' : 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==',
+    'uri': 'https://localhost:8081',
+}
