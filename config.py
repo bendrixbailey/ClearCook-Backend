485931ce72bfde48032a779d23fff701ee20eed5
Bendrix <32042393+bnb4505@users.noreply.github.com>
2022-06-07 08:34:09 -0400

    Delete config.py
    had some sensitive information inside

--- config.py
+++
@@ -1,8 +0,0 @@
-#port: 3306
-
-settings = {
-    'host' : 'clearcookdb.mongo.cosmos.azure.com',
-    'username' : 'clearcookdb',
-    'primary_key' : 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==',
-    'uri': 'https://clearcookdb.documents.azure.com:443/',
-}
